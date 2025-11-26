ae3b52afb0301c59d568793c94bf37f2452fd3d1
Ludo de Bergeyck <ludo.bergeyck@gmail.com>
2025-11-26 09:42:36 +0100

    promp is good

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -12,21 +12,7 @@ class QuizzesController < ApplicationController
     @level = @quiz.level
     @theme = @quiz.theme
     chat = RubyLLM.chat
-    @response = chat.ask("create a questionnaire with #{@number_of_questions} questions and their answers on the subject of #{@theme},  the questionnaire should have a difficulty level of #{@level} please give your response in the format of a array of hash [
-  {
-    question: 'Quelle est la capitale de la France ?',
-    reponse:  'Paris'
-  },
-  {
-    question: 'Combien font 2 + 2 ?',
-    reponse:  '4'
-  },
-  {
-    question: 'Qui a écrit 'Le Petit Prince' ?',
-    reponse:  'Antoine de Saint-Exupéry'
-  }
-] ne rajoute pas de saut de ligne /n")
-    raise
+    @response = chat.ask("create a questionnaire with #{@number_of_questions} questions and their answers on the subject of #{@theme},  the questionnaire should have a difficulty level of #{@level} please give your response in the format of a array of hash that I can parse with JSON.parse : [ { question: 'Quelle est la capitale de la France ?', reponse: 'Paris' }, { question: 'Combien font 2 + 2 ?', reponse: '4'}, { question: 'Qui a écrit 'Le Petit Prince' ?', reponse: 'Antoine de Saint-Exupéry' } ]. do not add any linebreak in your response")
     @quiz.save
   end
 
