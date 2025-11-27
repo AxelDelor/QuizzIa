177739f75b3d0968985bdf4a3772c678f0b2a1d0
Ludo de Bergeyck <ludo.bergeyck@gmail.com>
2025-11-27 11:51:54 +0100

    seed faites + lien de redirection vers la pagee result mais pas encore fonctionnel

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# puts "Creating theme..."
+# vache = Theme.create!(name: "Vache")
+
+Quiz.create(
+  theme: "Vache",
+  level: "medium",
+  number_of_questions: 3,
+  user_id: 1,
+)
+
+Quiz.create(
+  theme: "Sports",
+  level: "hard",
+  number_of_questions: 8,
+  user_id: 1,
+)
+
+Quiz.create(
+  theme: "Animal",
+  level: "easy",
+  number_of_questions: 10,
+  user_id: 1,
+)
