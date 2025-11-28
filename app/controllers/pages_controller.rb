5a2e607d0f237710c78ae84e08f2b7b0d33a5523
adil meftoh <aameftoh@gmail.com>
2025-11-28 15:42:26 +0100

    pages de profiles opé

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def profile
     @user = current_user
-    
+    @quizzes = @user.quizzes
   end
 end
